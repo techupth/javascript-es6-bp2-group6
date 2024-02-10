a2c364052365865146b6463a7b19d4863e536816
Jirayu <kraiwilat@gmail.com>
2024-02-10 20:55:33 +0700

    fe2-chit-zong-nonae-gap

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,10 @@
-let height = undefined;
-let result;
-
-console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
+function checkHeight(height) {
+    const result = height ?? "Height is not defined";
+    return result;
+  }
+  
+  let height = undefined;
+  let result = checkHeight(height);
+  
+  console.log(result);
+  
